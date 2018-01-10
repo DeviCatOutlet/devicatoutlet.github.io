d2aa1baf68a09c1ac94ad4922b364afd0489223a
Chris Angelico <rosuav@gmail.com>
2018-01-10 13:53:26 +1100

    Suppress the alt-text captions for now

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#gallery ~ p img').click(function() {
         $('.imagepreview').attr('src', $(this).attr('src'));
-        $('#modal-caption').text($(this).attr('alt'));
+        //$('#modal-caption').text($(this).attr('alt')); //TODO: Style these up nicely and then enable them
         $('#imagemodal').modal('show');
     });
 });
