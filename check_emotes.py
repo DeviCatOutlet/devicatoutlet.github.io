1a74aacabc4b90d1974083de656884c08b7faa81
Chris Angelico <rosuav@gmail.com>
2019-11-15 20:00:17 +1100

    En-repo a script to help notice when emotes change too drastically

---
+++ check_emotes.py
@@ -0,0 +1,23 @@
+import subprocess
+
+block = []
+for line in subprocess.run(["git", "diff"], capture_output=True, check=True, encoding="utf-8").stdout.split("\n"):
+	if line.startswith("diff --git"):
+		# Header line
+		print(line)
+	if line.startswith("-"):
+		block.append(line)
+	elif line.startswith("+"):
+		if not block:
+			print(line) # Added line
+			continue
+		changedfrom = block.pop(0)
+		delta = len(line) - len(changedfrom)
+		if 0 <= delta <= 5: continue # Accept small lengthenings, eg when emote IDs change
+		print(changedfrom)
+		print(line)
+	else:
+		# End of a block
+		for removed in block:
+			print(removed)
+		block = []
